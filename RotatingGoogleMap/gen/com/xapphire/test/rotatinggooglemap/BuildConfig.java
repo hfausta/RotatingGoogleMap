467d4b35be4e2cfb149d131319a938fbf0dfa380
HFausta <henry.fausta@gmail.com>
2014-01-02 14:49:19 +0700

    created new android application project

---
+++ RotatingGoogleMap/gen/com/xapphire/test/rotatinggooglemap/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.xapphire.test.rotatinggooglemap;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
