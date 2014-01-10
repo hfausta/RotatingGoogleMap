645de17802d77e4fe8152ab24e47d4544850ba95
HFausta <henry.fausta@gmail.com>
2014-01-11 00:27:04 +0700

    added sensor manager to detect phone movements

--- RotatingGoogleMap/src/com/xapphire/test/rotatinggooglemap/MainActivity.java
+++ RotatingGoogleMap/src/com/xapphire/test/rotatinggooglemap/MainActivity.java
@@ -1,17 +1,49 @@
 package com.xapphire.test.rotatinggooglemap;
+/*
+ * http://www.codingforandroid.com/2011/01/using-orientation-sensors-simple.html
+ */
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements SensorEventListener {
 
+	private GoogleMap googleMap;
+	private SensorManager sensorManager;
+	private Sensor accelerometer;
+	private Sensor magneticField;
+	private float[] accelerometerValues;
+	private float[] magneticFieldValues;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeMap();
+        googleMap.getUiSettings().setCompassEnabled(true);
+        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        
+        /*googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder()
+        .target(googleMap.getCameraPosition().target)
+        .zoom(googleMap.getCameraPosition().zoom)
+        .bearing(event.values[0])
+        .build()));*/
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -20,4 +52,57 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    private void initializeMap() {
+    	if(googleMap == null) {
+    		googleMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+    	}
+    }
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		/*
+		 * http://www.codingforandroid.com/2011/01/using-orientation-sensors-simple.html
+		 */
+		if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) accelerometerValues = event.values;
+		if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) magneticFieldValues = event.values;
+		
+		if(accelerometerValues != null && magneticFieldValues != null) {
+			float R[] = new float[9];
+			float I[] = new float[9];
+			boolean success = SensorManager.getRotationMatrix(R, I, accelerometerValues, magneticFieldValues);
+			
+			if(success) {
+				float orientation[] = new float[3];
+				SensorManager.getOrientation(R, orientation);
+				float bearingRotation = -360*orientation[0]/(2*3.14159f);
+				Toast.makeText(this, "Rotation " + bearingRotation, Toast.LENGTH_LONG).show();
+				
+				googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder()
+		        .target(googleMap.getCameraPosition().target)
+		        .zoom(googleMap.getCameraPosition().zoom)
+		        .bearing(0)
+		        .build()));
+				
+			}
+		}
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+		sensorManager.registerListener(this, magneticField, SensorManager.SENSOR_DELAY_UI);
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		sensorManager.unregisterListener(this);
+	}
 }
